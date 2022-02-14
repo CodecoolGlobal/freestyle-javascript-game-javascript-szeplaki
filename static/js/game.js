5e97d3820b4b8123122abc46febcb1dc09c7cfa5
Szeplaki <szeplaki.eva1@gmail.com>
2022-02-14 22:46:52 +0100

    create board

--- static/js/game.js
+++ static/js/game.js
@@ -1,7 +1,30 @@
-initGame();
-
 function initGame() {
-
+    createBoard()
     // Your game can start here, but define separate functions, don't write everything in here :)
-
 }
+
+function createBoard(){
+    let center = document.createElement('center');
+    let Board = document.createElement('table');
+    for (let i = 0; i < 15; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < 15; j++) {
+            let td = document.createElement('td');
+            if ((i + j) % 2 === 0) {
+                td.setAttribute('class', 'cell mediumseagreen-cell');
+                tr.appendChild(td);
+            } else {
+                td.setAttribute('class', 'cell lightgreen-cell');
+                tr.appendChild(td);
+            }
+        }
+        Board.appendChild(tr);
+    }
+    center.appendChild(Board);
+
+    // Modifying table attribute properties
+    Board.setAttribute('cellspacing', '0');
+    Board.setAttribute('width', '800px');
+    document.body.appendChild(center);
+}
+initGame();
