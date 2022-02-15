54040857e91502f69053d7d89a7405e96f988fbc
gborbely <gborbely766@gmail.com>
2022-02-15 14:22:36 +0100

    add menu buttons to navigate in menu or start game

--- static/js/game.js
+++ static/js/game.js
@@ -1,5 +1,5 @@
 function initGame() {
-    createBoard()
+    createMenu();
     // Your game can start here, but define separate functions, don't write everything in here :)
 }
 
@@ -26,5 +26,72 @@ function createBoard(){
     Board.setAttribute('cellspacing', '0');
     Board.setAttribute('width', '800px');
     document.body.appendChild(center);
+}
+
+function createMenu(){
+    let menu = document.createElement('div');
+    menu.className = "menu";
+    let menuOption1 = document.createElement('div');
+    menuOption1.className = "option";
+    let startGame = document.createElement('button');
+    startGame.id = 'Start';
+    startGame.textContent = 'Start Game';
+    startGame.onclick = (e)=> {chosenEvent(e)};
+    menuOption1.appendChild(startGame);
+    let menuOption2 = document.createElement('div');
+    menuOption2.className = "option";
+    let highScore = document.createElement('button');
+    highScore.id = 'high-score';
+    highScore.textContent = 'Highest Scores';
+    highScore.onclick = (e) => {chosenEvent(e)};
+    menuOption2.appendChild(highScore)
+    let menuOption3 = document.createElement('div');
+    menuOption3.className = "option";
+    let credits = document.createElement('button');
+    credits.id = 'credits';
+    credits.textContent = 'Credits';
+    credits.onclick = (e) => {chosenEvent(e)};
+    menuOption3.appendChild(credits)
+    menu.append(menuOption1);
+    menu.append(menuOption2);
+    menu.append(menuOption3);
+    document.body.appendChild(menu);
+}
+
+function chosenEvent(e){
+    if (e.target.id === 'Start'){
+        removeMenu();
+        createBoard();
+    }else if (e.target.id === 'high-score'){
+        removeMenu();
+        displayHighScore();
+
+    }else if (e.target.id === 'credits'){
+        removeMenu();
+        displayCredit();
+    }
+}
+
+function removeMenu() {
+    let menu = document.getElementsByClassName('menu');
+    while (menu.length > 0) {
+        menu[0].remove();
+        }
+
+}
+
+function displayHighScore(){
+    removeMenu();
+    let pTag = document.createElement('h1');
+    pTag.textContent = "highscores"
+    document.body.appendChild(pTag);
+
+}
+
+function displayCredit(){
+    removeMenu();
+    let pTag2 = document.createElement('p');
+    pTag2.textContent = "credits";
+    document.body.appendChild(pTag2);
 }
 initGame();
